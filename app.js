97e95bcc0838e5fdf1a147d238dddcb8a0b6fd6f
Saad Sheikh <saadsheikh27@gmail.com>
2020-12-23 23:59:58 +0500

    plsss

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ const port = process.env.PORT || 80;
 
 app.use(express.static("public"));
 
+const server = app.listen(port, () => {
 
+    console.log("server running on " + port)
+
+
+})
 
 
 
@@ -25,33 +30,11 @@ const io = socket(server);
 io.on("connection", (socket) => {
 
 
-    console.log("user connected ", socket.id)
-
-
-    socket.on("message", (data) => {
-
-        console.log("......")
-        io.emit("response", data)
-
-    })
-
-
-    socket.on("disconnect", () => {
-
-
-        console.log("2221321333332222")
-
-    })
-
-
-
-
+    console.log("connected")
+    socket.on('data', data => {
+        io.emit('client', data);
+        console.log(data);
+    });
 
 })
 
-const server = app.listen(port, () => {
-
-    console.log("server running on " + port)
-
-
-})
